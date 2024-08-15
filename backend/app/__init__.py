5ec0be94ad7f94e50431eb6fa990b29a5f1ee396
will-wagura <wwagura0@gmail.com>
2024-08-15 05:00:01 +0300

    Creates the backend structure

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,8 +9,7 @@ from flask_bcrypt import Bcrypt
 from flask_socketio import SocketIO
 
 # Initialize Flask extensions
-from .models import db, User, Role
-
+db = SQLAlchemy()
 jwt = JWTManager()
 mail = Mail()
 migrate = Migrate()
@@ -18,18 +17,15 @@ cors = CORS()
 bcrypt = Bcrypt()
 socketio = SocketIO()
 
-
 def create_app():
-
     app = Flask(__name__)
-    app.config.from_object("app.config.Config")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    app.config.from_object('app.config.Config')
 
-    # Initialize database and migration with the already imported db
+    # Initialize database and migration
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate = Migrate(app, db)
 
-    # Initialize Bcrypt
+    #Initialize Bcrypt
     bcrypt.init_app(app)
 
     # Setup Flask-Mail
@@ -38,11 +34,14 @@ def create_app():
     # Setup JWT
     jwt.init_app(app)
 
-    # Setup CORS
-    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
+    # Setup Cors
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     # Initialize SocketIO
     socketio.init_app(app, cors_allowed_origins="*")
+
+    #Import Models
+    from .models import User, Role
 
     # Setup Flask-Security
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
@@ -50,17 +49,7 @@ def create_app():
 
     # Register blueprints
     with app.app_context():
-        from app.routes import (
-            auth_routes,
-            email_routes,
-            image_routes,
-            payment_routes,
-            product_routes,
-            report_routes,
-            supplies_routes,
-            users_routes,
-            chat_routes,
-        )
+        from app.routes import auth_routes, email_routes, image_routes, payment_routes, product_routes, report_routes, supplies_routes, users_routes, chat_routes
 
         app.register_blueprint(auth_routes.bp)
         app.register_blueprint(email_routes.bp)
@@ -72,7 +61,7 @@ def create_app():
         app.register_blueprint(users_routes.users_routes)
         app.register_blueprint(chat_routes.chat_bp)
 
-        # Create database tables
+        #Create database tables
         db.create_all()
 
     return app
--- backend/app/config.py
+++ backend/app/config.py
@@ -4,47 +4,40 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def generate_jwt_secret_key():
     jwt_secret_key = secrets.token_urlsafe(16)
-    with open(".env", "a") as f:
-        f.write(f"JWT_SECRET_KEY={jwt_secret_key}\n")
-
+    with open('.env', 'a') as f:
+        f.write(f'JWT_SECRET_KEY={jwt_secret_key}\n')
 
 def load_or_generate_jwt_secret_key():
-    if not os.getenv("JWT_SECRET_KEY"):
-        if os.path.exists(".env"):
-            with open(".env", "r") as f:
+    if not os.getenv('JWT_SECRET_KEY'):
+        if os.path.exists('.env'):
+            with open('.env', 'r') as f:
                 lines = f.readlines()
                 for line in lines:
-                    if line.startswith("JWT_SECRET_KEY="):
+                    if line.startswith('JWT_SECRET_KEY='):
                         return
         generate_jwt_secret_key()
 
-
 load_or_generate_jwt_secret_key()
 
-
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT")
-    # SECURITY_PASSWORD_SALT = "your_secret_salt"
-    SECURITY_PASSWORD_HASH = "bcrypt"
-
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
+    
     # Flask-Mail settings
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = int(os.getenv("MAIL_PORT") or 25)
-    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") is not None
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
-
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+    
     # URL of the frontend application
-    FRONTEND_URL = os.getenv("FRONTEND_URL")
-
+    FRONTEND_URL = os.getenv('FRONTEND_URL')
 
 # class DevelopmentConfig(Config):
 #     DEBUG = True
@@ -62,9 +55,8 @@ class Config:
 #     'production': ProductionConfig
 # }
 
-
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     MAIL_SUPPRESS_SEND = True
     WTF_CSRF_ENABLED = False
